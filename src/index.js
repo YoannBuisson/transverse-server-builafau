ad0da61690d4ab1c978cc5ce17e2ee7237efc387
Yoann <yoann.buisson@etu.uca.fr>
2020-05-21 00:10:20 +0200

    Added schemas

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
-import { ApolloServer,gql } from 'apollo-server';
+import {ApolloServer, gql} from 'apollo-server';
 
 const typeDefs = gql`
     # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
     # This "Book" type defines the queryable fields for every book in our data source.
-    type Book {
-        title: String
-        author: String
+    type User {
+        name: String
+        surname: String
+        dateOfBirth: String
+        friends: [User]
+    }
+
+    type Task {
+        name: String
+        duration: Int
+        priority: Int
+        assignee: User
+    }
+
+    type Project {
+        name: String
+        creator: User
+        tasks: [Task]
     }
 
     # The "Query" type is special: it lists all of the available queries that
     # clients can execute, along with the return type for each. In this
     # case, the "books" query returns an array of zero or more Books (defined above).
     type Query {
-        books: [Book]
+        users: [User]
+        tasks: [Task]
+        projects: [Project]
     }
 `;
 
 
-const books = [
+const users = [
     {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
+        name: "Doe",
+        surname: "John",
+        login: "user1",
+        pass: "password1"
     },
     {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton',
+        name: "C. Harden",
+        surname: "James",
+        login: "user2",
+        pass: "password2"
     },
 ];
 
@@ -34,15 +55,15 @@ const books = [
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-        books: () => books,
+        users: () => users,
     },
 };
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({typeDefs, resolvers});
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+server.listen().then(({url}) => {
     console.log(`🚀  Server ready at ${url}`);
 });
